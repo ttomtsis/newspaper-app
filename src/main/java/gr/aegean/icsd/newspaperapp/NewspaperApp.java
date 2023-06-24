9e55ee3fb7720fe98f3285a141c15d9878e83bdc
Tomtsis T <ttomtsis@protonmail.com>
2023-06-24 21:07:57 +0300

    First Commit

---
+++ src/main/java/gr/aegean/icsd/newspaperapp/NewspaperApp.java
@@ -0,0 +1,13 @@
+package gr.aegean.icsd.newspaperapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewspaperApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewspaperApp.class, args);
+    }
+
+}
---
+++ src/test/java/gr/aegean/icsd/newspaperapp/NewspaperAppTests.java
@@ -0,0 +1,13 @@
+package gr.aegean.icsd.newspaperapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewspaperAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
