04e145d47f4de58609f3a845d0274eafc956072b
Tomtsis T <ttomtsis@protonmail.com>
2023-07-03 10:46:41 +0300

    Added documentation

--- src/main/java/gr/aegean/icsd/newspaperapp/util/enums/CommentState.java
+++ src/main/java/gr/aegean/icsd/newspaperapp/util/enums/CommentState.java
@@ -1,3 +1,19 @@
 package gr.aegean.icsd.newspaperapp.util.enums;
 
-public enum CommentState { SUBMITTED, APPROVED }
+/**
+ * Comment's possible states <br>
+ * {@link #SUBMITTED} <br>
+ * {@link #APPROVED} <br>
+ */
+public enum CommentState {
+    /**
+     * Initial state when a comment is created, <br>
+     * currently not published and can be modified
+     */
+    SUBMITTED,
+
+    /**
+     * An APPROVED comment is published and accessible by all users
+     */
+    APPROVED
+}
