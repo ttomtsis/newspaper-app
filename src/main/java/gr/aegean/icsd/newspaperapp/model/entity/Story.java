434f98bc47393c51c140fb80d8646ec7b2a372df
Tomtsis T <ttomtsis@protonmail.com>
2023-07-07 21:35:39 +0300

    Using mappedBy in @OneToMany

--- src/main/java/gr/aegean/icsd/newspaperapp/model/entity/Story.java
+++ src/main/java/gr/aegean/icsd/newspaperapp/model/entity/Story.java
@@ -1,8 +1,9 @@
 package gr.aegean.icsd.newspaperapp.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Story {
@@ -10,11 +11,39 @@ public class Story {
     @GeneratedValue
     private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    /**
+     * Comments associated with the Story. <br>
+     */
+    @OneToMany(mappedBy = "storyID", targetEntity = Comment.class,
+            cascade = {CascadeType.REFRESH, CascadeType.REMOVE}, orphanRemoval = true)
+    private Set<Comment> commentsList = new HashSet<Comment>();
+
+    /**
+     * Author of the Story. <br>
+     * Many Stories can have the same Author <br>
+     */
+//    @ManyToOne(cascade = CascadeType.REFRESH)
+//    @JoinColumn(name = "authorID", nullable = false, updatable = false)
+//    private User author;
+
+    public Story(){}
 
     public Long getId() {
         return id;
     }
+
+    public void addComment(Comment newComment) {
+        if (newComment != null) {
+            commentsList.add(newComment);
+        }
+        else {
+            throw new RuntimeException("Null comment cannot be added to the comments list");
+        }
+    }
+
+    public Set<Comment> getComments() {
+        return this.commentsList;
+    }
+
+
 }
