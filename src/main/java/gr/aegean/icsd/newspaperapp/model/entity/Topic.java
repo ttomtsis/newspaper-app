89785a33c10dfc3efbf11d4b00d6c9da2eec27b8
Tomtsis T <ttomtsis@protonmail.com>
2023-07-08 17:00:17 +0300

    Added author foreign key

--- src/main/java/gr/aegean/icsd/newspaperapp/model/entity/Topic.java
+++ src/main/java/gr/aegean/icsd/newspaperapp/model/entity/Topic.java
@@ -1,14 +1,23 @@
 package gr.aegean.icsd.newspaperapp.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "topic")
 public class Topic {
+
     @Id
     @GeneratedValue
     private Long id;
+
+    /**
+     * Author of the Topic. <br>
+     * Many Topics can have the same Author <br>
+     * Only one Author per Topic is allowed
+     */
+    @ManyToOne(cascade = CascadeType.REFRESH, targetEntity = User.class)
+    @JoinColumn(name = "authorID", nullable = false, updatable = false)
+    private User authorID;
 
     public void setId(Long id) {
         this.id = id;
