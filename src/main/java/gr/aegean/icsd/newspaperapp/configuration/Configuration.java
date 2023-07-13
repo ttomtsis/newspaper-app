4811c5f86b6abe93df3c1deb14802281f9f560f9
Tomtsis T <ttomtsis@protonmail.com>
2023-07-13 18:50:36 +0300

    Implemented create users to preload two users in the database

--- src/main/java/gr/aegean/icsd/newspaperapp/configuration/Configuration.java
+++ src/main/java/gr/aegean/icsd/newspaperapp/configuration/Configuration.java
@@ -1,6 +1,8 @@
 package gr.aegean.icsd.newspaperapp.configuration;
 
+import gr.aegean.icsd.newspaperapp.model.entity.User;
 import gr.aegean.icsd.newspaperapp.model.repository.UserRepository;
+import gr.aegean.icsd.newspaperapp.util.enums.UserType;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -18,12 +20,22 @@ public class Configuration {
     }
 
     /**
-     * Pre-loads the database with Users
-     * @return True if users created successfully
+     * Pre-loads the database with Users <br>
+     *
+     * @return True
      */
     @Bean
     @Transactional
     public boolean createUsers() {
+
+        if (repository.findByUsername("testCurator").isEmpty()) {
+            repository.save(new User("testCurator", "password", UserType.CURATOR));
+        }
+
+        if (repository.findByUsername("testJournalist").isEmpty()) {
+            repository.save(new User("testJournalist", "password", UserType.JOURNALIST));
+        }
+
         return true;
     }
 
