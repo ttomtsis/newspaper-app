10e6f335c6af48779575e51cae14a9293d709c15
Tomtsis T <ttomtsis@protonmail.com>
2023-07-18 15:57:32 +0300

    Enabled method security

--- src/main/java/gr/aegean/icsd/newspaperapp/configuration/SecurityConfiguration.java
+++ src/main/java/gr/aegean/icsd/newspaperapp/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -29,6 +30,7 @@ import java.util.Arrays;
 
 import static jakarta.servlet.DispatcherType.*;
 @Configuration
+@EnableMethodSecurity
 @EnableWebSecurity
 public class SecurityConfiguration {
 
